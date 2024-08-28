be4a1cf022c45343ea280e28ec44e975391a2b59
theKuanWay <liewkw94@gmail.com>
2024-08-28 20:36:10 +0800

    add codes for qn3

--- assignment/q3-objects.js
+++ assignment/q3-objects.js
@@ -12,8 +12,7 @@
 */
 
 const bookIdArr = ["NLB1", "NLB2", "NLB3", "NLB4"];
-const bookTitle = ["Lord of the Rings", "Programming for Dummies", "Introduction to Software Testing", "How to be a Software Developer"];
-let booksObj = {}; 
+const bookTitle = ["Lord of the Rings", "Programming for Dummies", "Introduction to Software Testing", "How to be a Software Developer"]; 
 
 function convert(keyArr, valueArr){
     
@@ -25,6 +24,12 @@ function convert(keyArr, valueArr){
         - Step 3: Within the for-loop, add the key and value to the local scoped object.
         - Step 4: Write a return statement to return the object literal after the for-loop code block.
     */
+    let booksObj = {}; //empty array
+
+    for (let i = 0; i < keyArr.length; i++) {
+        booksObj[keyArr[i]] = valueArr[i];
+    }
+    return booksObj;
 }
 
 function printByKey(key){
